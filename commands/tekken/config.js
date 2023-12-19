44e831603875122a2a1316602c297aae0129495a
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2023-12-19 13:59:57 +0100

    Refactor config.js

--- commands/tekken/config.js
+++ commands/tekken/config.js
@@ -53,172 +53,88 @@ module.exports = function getCharacterCode(character) {
     zafina: "zafina",
   };
 
+  const createAliasMap = (aliases, code) => {
+    return aliases.reduce((map, alias) => {
+      map[alias] = code;
+      return map;
+    }, {});
+  };
+
   const characterMap = {
-    gouki: characterCodes.akuma,
-    akuma: characterCodes.akuma,
-    aku: characterCodes.akuma,
-
-    alisa: characterCodes.alisa,
-    alis: characterCodes.alisa,
-    als: characterCodes.alisa,
-
-    anna: characterCodes.anna,
-    ann: characterCodes.anna,
-
-    armorking: characterCodes.armor_king,
-    ak: characterCodes.armor_king,
-    amk: characterCodes.armor_king,
-
-    asuka: characterCodes.asuka,
-
-    bob: characterCodes.bob,
-
-    bryan: characterCodes.bryan,
-    bry: characterCodes.bryan,
-    brain: characterCodes.bryan,
-    brian: characterCodes.bryan,
-
-    claudio: characterCodes.claudio,
-    cla: characterCodes.claudio,
-
-    deviljin: characterCodes.devil_jin,
-    devil: characterCodes.devil_jin,
-    dj: characterCodes.devil_jin,
-
-    dragunov: characterCodes.dragunov,
-    drag: characterCodes.dragunov,
-    dra: characterCodes.dragunov,
-
-    eddy: characterCodes.eddy,
-    eddie: characterCodes.eddy,
-    edy: characterCodes.eddy,
-    edd: characterCodes.eddy,
-
-    eliza: characterCodes.eliza,
-    liz: characterCodes.eliza,
-    elisa: characterCodes.eliza,
-    liza: characterCodes.eliza,
-    elz: characterCodes.eliza,
-
-    fahkumram: characterCodes.fahkumram,
-    fahk: characterCodes.fahkumram,
-    fak: characterCodes.fahkumram,
-    fhk: characterCodes.fahkumram,
-
-    feng: characterCodes.feng,
-    fng: characterCodes.feng,
-
-    ganryu: characterCodes.ganryu,
-    ganny: characterCodes.ganryu,
-    gan: characterCodes.ganryu,
-    gry: characterCodes.ganryu,
-
-    geese: characterCodes.geese,
-    howard: characterCodes.geese,
-
-    gigas: characterCodes.gigas,
-    gigass: characterCodes.gigas,
-
-    heihachi: characterCodes.heihachi,
-    hei: characterCodes.heihachi,
-
-    hwoarang: characterCodes.hwoarang,
-    hwo: characterCodes.hwoarang,
-
-    "jack-7": characterCodes.jack_7,
-    jack: characterCodes.jack_7,
-    jack7: characterCodes.jack_7,
-    jack_7: characterCodes.jack_7,
-
-    jin: characterCodes.jin,
-
-    josie: characterCodes.josie,
-    crybaby: characterCodes.josie,
-
-    julia: characterCodes.julia,
-    chang: characterCodes.julia,
-    jul: characterCodes.julia,
-
-    katarina: characterCodes.katarina,
-    kat: characterCodes.katarina,
-
-    kazumi: characterCodes.kazumi,
-    kzm: characterCodes.kazumi,
-
-    kaz: characterCodes.kazuya,
-    kazuya: characterCodes.kazuya,
-
-    king: characterCodes.king,
-
-    kuma: characterCodes.kuma,
-    panda: characterCodes.panda,
-    bear: characterCodes.kuma,
-    bears: characterCodes.kuma,
-
-    kunimitsu: characterCodes.kunimitsu,
-    kuni: characterCodes.kunimitsu,
-
-    lars: characterCodes.lars,
-
-    law: characterCodes.law,
-    bruce_lee: characterCodes.law,
-    "bruce-lee": characterCodes.law,
-
-    lee: characterCodes.lee,
-
-    lei: characterCodes.lei,
-    jackie_chan: characterCodes.lei,
-    "jackie-chan": characterCodes.lei,
-
-    leo: characterCodes.leo,
-
-    leroy: characterCodes.leroy,
-
-    lidia: characterCodes.lidia,
-
-    lili: characterCodes.lili,
-
-    lucky_chloe: characterCodes.lucky_chloe,
-    chloe: characterCodes.lucky_chloe,
-    lucky: characterCodes.lucky_chloe,
-    idol: characterCodes.lucky_chloe,
-
-    marduk: characterCodes.marduk,
-    duk: characterCodes.marduk,
-    craig: characterCodes.marduk,
-
-    master_raven: characterCodes.master_raven,
-    "master-raven": characterCodes.master_raven,
-    maven: characterCodes.master_raven,
-    raven: characterCodes.master_raven,
-
-    miguel: characterCodes.miguel,
-    mig: characterCodes.miguel,
-
-    negan: characterCodes.negan,
-
-    nina: characterCodes.nina,
-
-    noctis: characterCodes.noctis,
-
-    paul: characterCodes.paul,
-
-    shaheen: characterCodes.shaheen,
-
-    steve: characterCodes.steve,
-    boxer: characterCodes.steve,
-
-    xiaoyu: characterCodes.xiaoyu,
-    xioayu: characterCodes.xiaoyu,
-    ling: characterCodes.xiaoyu,
-    x: characterCodes.xiaoyu,
-
-    yoshimitsu: characterCodes.yoshimitsu,
-    yoshi: characterCodes.yoshimitsu,
-    yosh: characterCodes.yoshimitsu,
-
-    zafina: characterCodes.zafina,
-    zaf: characterCodes.zafina,
+    ...createAliasMap(["akuma", "gouki", "aku"], characterCodes.akuma),
+    ...createAliasMap(["alisa", "alis", "als"], characterCodes.alisa),
+    ...createAliasMap(["anna", "ann"], characterCodes.anna),
+    ...createAliasMap(
+      ["armorking", "armor-king", "armor_king", "ak", "amk"],
+      characterCodes.armor_king
+    ),
+    ...createAliasMap(["asuka"], characterCodes.asuka),
+    ...createAliasMap(["bob"], characterCodes.bob),
+    ...createAliasMap(["bryan", "bry", "brain", "brian"], characterCodes.bryan),
+    ...createAliasMap(["claudio", "cla"], characterCodes.claudio),
+    ...createAliasMap(
+      ["devil-jin", "devil_jin", "deviljin", "devil", "dj"],
+      characterCodes.devil_jin
+    ),
+    ...createAliasMap(["dragunov", "drag", "dra"], characterCodes.dragunov),
+    ...createAliasMap(["eddy", "eddie", "edy", "edd"], characterCodes.eddy),
+    ...createAliasMap(
+      ["eliza", "liz", "elisa", "liza", "elz"],
+      characterCodes.eliza
+    ),
+    ...createAliasMap(
+      ["fahkumram", "fahk", "fak", "fhk"],
+      characterCodes.fahkumram
+    ),
+    ...createAliasMap(["feng", "fng"], characterCodes.feng),
+    ...createAliasMap(["ganryu", "gan", "gry"], characterCodes.ganryu),
+    ...createAliasMap(["geese", "howard"], characterCodes.geese),
+    ...createAliasMap(["gigas", "gigass"], characterCodes.gigas),
+    ...createAliasMap(["heihachi", "hei"], characterCodes.heihachi),
+    ...createAliasMap(["hwoarang", "hwo"], characterCodes.hwoarang),
+    ...createAliasMap(
+      ["jack", "jack7", "jack_7", "jack-7"],
+      characterCodes.jack_7
+    ),
+    ...createAliasMap(["jin"], characterCodes.jin),
+    ...createAliasMap(["josie", "crybaby"], characterCodes.josie),
+    ...createAliasMap(["julia", "chang", "jul"], characterCodes.julia),
+    ...createAliasMap(["katarina", "kat"], characterCodes.katarina),
+    ...createAliasMap(["kazumi", "kzm"], characterCodes.kazumi),
+    ...createAliasMap(["kazuya", "kaz"], characterCodes.kazuya),
+    ...createAliasMap(["king"], characterCodes.king),
+    ...createAliasMap(["kuma", "panda", "bear", "bears"], characterCodes.kuma),
+    ...createAliasMap(["kunimitsu", "kuni"], characterCodes.kunimitsu),
+    ...createAliasMap(["lars"], characterCodes.lars),
+    ...createAliasMap(["law", "bruce_lee", "bruce-lee"], characterCodes.law),
+    ...createAliasMap(["lee", "flower"], characterCodes.lee),
+    ...createAliasMap(
+      ["lei", "jackie_chan", "jackie-chan"],
+      characterCodes.lei
+    ),
+    ...createAliasMap(["leo"], characterCodes.leo),
+    ...createAliasMap(["leroy"], characterCodes.leroy),
+    ...createAliasMap(["lidia"], characterCodes.lidia),
+    ...createAliasMap(["lili"], characterCodes.lili),
+    ...createAliasMap(
+      ["luckychloe", "lucky-chloe", "lucky_chloe", "chloe", "lucky", "idol"],
+      characterCodes.lucky_chloe
+    ),
+    ...createAliasMap(["marduk", "duk", "craig"], characterCodes.marduk),
+    ...createAliasMap(
+      ["masterraven", "master-raven", "maven", "raven"],
+      characterCodes.master_raven
+    ),
+    ...createAliasMap(["miguel", "mig"], characterCodes.miguel),
+    ...createAliasMap(["negan"], characterCodes.negan),
+    ...createAliasMap(["nina"], characterCodes.nina),
+    ...createAliasMap(["noctis"], characterCodes.noctis),
+    ...createAliasMap(["paul"], characterCodes.paul),
+    ...createAliasMap(["shaheen"], characterCodes.shaheen),
+    ...createAliasMap(["steve", "boxer"], characterCodes.steve),
+    ...createAliasMap(["xioayu", "ling", "x"], characterCodes.xiaoyu),
+    ...createAliasMap(["yoshi", "yosh", "yos"], characterCodes.yoshimitsu),
+    ...createAliasMap(["zafina", "zaf"], characterCodes.zafina),
   };
 
   character = character.toLowerCase();
