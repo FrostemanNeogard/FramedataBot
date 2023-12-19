d14aa36c2494543e75b47017524f19b0bd7e4809
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2023-12-19 13:47:15 +0100

    Refactor entire command

--- commands/tekken/framedata.js
+++ commands/tekken/framedata.js
@@ -9,28 +9,31 @@ module.exports = {
   permissions: [],
   devCommand: false,
   run: async ({ client, msg, args }) => {
-    // Get formatted character code
-    const characterCode = getCharacterCode(args[0]);
+    try {
+      // Get formatted character code
+      const characterCode = getCharacterCode(args[0]);
 
-    // Guard clause to check for non-existing characters
-    if (!characterCode) {
-      console.error(`Couldn't find character: ${args[0]}`);
-      return msg.reply(`Couldn't find character: "${args[0]}".`);
-    }
+      // Guard clause to check for non-existing characters
+      if (!characterCode) {
+        console.error(`Couldn't find character: ${args[0]}`);
+        return msg.reply(`Couldn't find character: "${args[0]}".`);
+      }
 
-    // Guard clause to check for missing notation
-    if (args.length < 2) {
-      console.error(`No notation given.`);
-      return msg.reply("Please provide a move in tekken notation.");
-    }
+      // Guard clause to check for missing notation
+      if (args.length < 2) {
+        console.error(`No notation given.`);
+        return msg.reply("Please provide a move in tekken notation.");
+      }
 
-    const rbnUrl = `https://rbnorway.org/${characterCode}-t7-frames/`;
-    console.log("Attempting to fetch data from:", rbnUrl);
+      // Build RBNorway URL for character frames
+      const rbnUrl = `https://rbnorway.org/${characterCode}-t7-frames/`;
+      console.log("Attempting to fetch data from:", rbnUrl);
 
-    // Fetch rbnUrl
-    needle.get(rbnUrl, (err, res) => {
+      // Fetch RBNorway data
+      const { body } = await needle("get", rbnUrl);
+
       // Guard clause if fetch was unsuccessful
-      if (err || res.statusCode !== 200) {
+      if (!body || body.statusCode !== 200) {
         console.error("Couldn't fetch data.");
         return msg.reply(
           "An error occurred when fetching data. Please try again later."
@@ -38,7 +41,7 @@ module.exports = {
       }
 
       // RBNorway body
-      const $ = cheerio.load(res.body, { decodeEntities: false });
+      const $ = cheerio.load(body, { decodeEntities: false });
 
       // All args after the character name get stored as the unformatted inputs
       const unformattedInputs = args.slice(1).join("");
@@ -46,7 +49,7 @@ module.exports = {
       // Attempt to find a matching input from RBNorway
       let matchedInputTr = checkForInput($, unformattedInputs);
 
-      // If not matching input was found, look again with additional leniency
+      // If no matching input was found, look again with additional leniency
       if (matchedInputTr.length < 1) {
         matchedInputTr = checkForInput($, unformattedInputs, true);
       }
@@ -75,12 +78,12 @@ module.exports = {
         .setColor(0x00ff00)
         .setTitle(formattedCharacterName)
         .setURL(rbnUrl)
-        .setDescription(`Move: ${attackInfo.input}`);
+        .setDescription(`Move: ${attackInfo.input}`)
+        .setFooter({
+          text: `Please report any issues to "${process.env.OWNER_NAME}" on discord.`,
+        });
 
-      responseEmbed.setFooter({
-        text: `Please report any issues to "${process.env.OWNER_NAME}" on discord.`,
-      });
-
+      // Define fields for response embed
       const fields = [
         { name: "Hit Level", value: attackInfo.hitLevel, inline: true },
         { name: "Damage", value: attackInfo.damage, inline: true },
@@ -101,31 +104,28 @@ module.exports = {
       // Finally, respond with the generated embed
       console.log("Replying with: ", responseEmbed);
       return msg.reply({ embeds: [responseEmbed] });
-    });
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+      return msg.reply("An error occurred. Please try again later.");
+    }
   },
 };
 
 // Function to format tekken notation to allow for easier parsing
 function formatNotation(inputNotation, removePlus) {
-  let modifiedNotation = inputNotation;
-  modifiedNotation = modifiedNotation.replace(/ *\([^)]*\) */g, "");
-  modifiedNotation = modifiedNotation.toLowerCase();
-  modifiedNotation = modifiedNotation.split("or");
-  modifiedNotation =
-    modifiedNotation.length === 0
-      ? modifiedNotation[0]
-      : modifiedNotation[modifiedNotation.length - 1];
-  modifiedNotation = modifiedNotation.split("in rage");
-  modifiedNotation =
-    modifiedNotation.length === 0
-      ? modifiedNotation[0]
-      : modifiedNotation[modifiedNotation.length - 1];
-  modifiedNotation = modifiedNotation.replaceAll(" ", "");
-  modifiedNotation = modifiedNotation.replaceAll(",", "");
-  modifiedNotation = modifiedNotation.replaceAll("/", "");
+  let modifiedNotation = inputNotation
+    .replace(/ *\([^)]*\) */g, "")
+    .toLowerCase()
+    .split("or")
+    .pop()
+    .split("in rage")
+    .pop()
+    .replace(/[\s,+/()]/g, "");
+
   if (removePlus) {
-    modifiedNotation = modifiedNotation.replaceAll("+", "");
+    modifiedNotation = modifiedNotation.replace(/\+/g, "");
   }
+
   return modifiedNotation;
 }
 
@@ -134,10 +134,10 @@ function checkForInput($, unformattedInputs, removePlus = false) {
   const formattedInputs = formatNotation(unformattedInputs, removePlus);
   const attackRow = $("tr:has(td)")
     .filter((index, element) => {
-      let tdText = $(element).find("td").first().text().trim().toLowerCase();
-      tdText = formatNotation(tdText, removePlus);
-      return tdText == formattedInputs;
+      const tdText = $(element).find("td").first().text().trim().toLowerCase();
+      return formatNotation(tdText, removePlus) == formattedInputs;
     })
     .first();
+
   return attackRow;
 }
