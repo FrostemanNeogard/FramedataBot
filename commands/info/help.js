047773500f766e0cc3910b2901c4e75e4476574f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2023-12-23 21:59:44 +0100

    Add help command

---
+++ commands/info/help.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "help",
+  category: "info",
+  permissions: [],
+  devCommand: false,
+  run: async ({ client, msg, args }) => {
+    // Reply with "Pong!"
+    const responseEmbed = new MessageEmbed()
+      .setColor(0x00ff00)
+      .setTitle("HELP")
+      .setDescription(`Command information`)
+      .setFields([
+        {
+          name: "!fd {characterName} {attackNotation}",
+          value:
+            "Responds with frame data for the given attack. (Tekken 8 data will be added as soon as possible)",
+        },
+        {
+          name: "!report {message}",
+          value: "Send feedback about this bot.",
+        },
+        {
+          name: "!help",
+          value: "Replies with information for all commands.",
+        },
+        {
+          name: "!ping",
+          value: "Responds with 'Pong!'",
+        },
+      ])
+      .setFooter({
+        text: `Please report any issues to "${process.env.OWNER_NAME}" on discord.`,
+      });
+    return msg.reply({ embeds: [responseEmbed] });
+  },
+};
