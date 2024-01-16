93083cc9dfaf917604d01201ee6e22a6270e99d0
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-01-16 22:50:15 +0100

    Update to accomodate discord.js v14 slash commands

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,8 +1,16 @@
+const { SlashCommandBuilder } = require("discord.js");
+
 module.exports = {
   name: "ping",
   category: "info",
   permissions: [],
   devCommand: false,
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction) {
+    await interaction.reply("Pong!");
+  },
   run: async ({ client, msg, args }) => {
     // Reply with "Pong!"
     return msg.reply("Pong!");
