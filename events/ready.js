cc1d3fe9ad64a93fdf042bf42e35260ac20830c5
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-01-16 22:50:55 +0100

    Add bot activity status

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,12 @@
 module.exports = {
   name: "ready",
   run: async (bot) => {
+    const { client } = bot;
+    client.user.setPresence({
+      activities: [{ name: "!help" }],
+      status: "online",
+    });
+
     // Log when bot goes online
     console.log(`${bot.client.user.tag} is now online!`);
   },
