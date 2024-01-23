9a7936048f5baedac3a68a7fe812ef378f12ecf4
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-01-23 13:13:24 +0100

    Refactor framedata command to work with newly created API

--- commands/tekken/framedata.js
+++ commands/tekken/framedata.js
@@ -1,7 +1,4 @@
-const cheerio = require("cheerio");
-const needle = require("needle");
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const getCharacterCode = require("./config/config");
 
 module.exports = {
   name: "fd",
@@ -46,77 +43,50 @@ module.exports = {
   run: async ({ client, msg, args }, slashCommand = false) => {
     try {
       if (args.length < 2) {
-        console.log("Missing args.");
+        console.error(`Missing args.`);
         return respond(
           msg,
-          "Please provide a character and an attack in proper notation. Use the help command for more info.",
+          `Please provide a move in tekken notation along with the character. You can use the "help" command for additional details.`,
           slashCommand
         );
       }
 
-      // Get formatted character code
-      const characterCode = getCharacterCode(args[0]);
-
-      // Guard clause to check for non-existing characters
-      if (!characterCode) {
-        console.error(`Couldn't find character: ${args[0]}`);
-        console.log("Couldn't find given character.");
-        return respond(
-          msg,
-          `Couldn't find character: "${args[0]}".`,
-          slashCommand
-        );
-      }
-
-      // Guard clause to check for missing notation
-      if (args.length < 2) {
-        console.error(`No notation given.`);
-        return respond(
-          msg,
-          "Please provide a move in tekken notation.",
-          slashCommand
-        );
-      }
-
-      // React to message to confirm everything went well
       if (!slashCommand) {
         msg.react("👀");
       }
+      const unformattedInputs = args.slice(1).join("");
+      const fetch = (await import("node-fetch")).default;
+      const characterCodeResponse = await fetch(
+        `http://localhost:3000/character-code/${args[0]}`
+      );
 
-      // Build RBNorway URL for character frames
-      const rbnUrl = `https://rbnorway.org/${characterCode}-t7-frames/`;
-      console.log("Attempting to fetch data from:", rbnUrl);
-
-      // Fetch RBNorway data
-      const { body, statusCode } = await needle("get", rbnUrl);
-
-      // Guard clause if fetch was unsuccessful
-      if (!body || statusCode !== 200) {
-        console.error("Couldn't fetch data.");
+      if (characterCodeResponse.status === 400) {
+        console.error("Couldn't find the given character.");
+        if (!slashCommand) {
+          msg.react("❌");
+        }
         return respond(
           msg,
-          "An error occurred when fetching data. Please try again later.",
+          `Couldn't find the given character: ${args[0]}.`,
           slashCommand
         );
       }
 
-      // RBNorway body
-      const $ = cheerio.load(body, { decodeEntities: false });
-
-      // All args after the character name get stored as the unformatted inputs
-      const unformattedInputs = args.slice(1).join("");
-
-      let matchedInputTr;
-      for (let i = 0; i < 2; i++) {
-        let doRemovePlus = i > 0;
-        matchedInputTr = checkForInput($, unformattedInputs, doRemovePlus);
-        if (matchedInputTr.length >= 1) {
-          break;
+      const characterCodeData = await characterCodeResponse.json();
+      const characterCode = characterCodeData.characterCode;
+      const frameDataResponse = await fetch(
+        `http://localhost:3000/tekken7/${characterCode}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            input: unformattedInputs,
+          }),
         }
-      }
+      );
+      const frameData = await frameDataResponse.json();
 
-      // Guard clause to check if a matched input was found
-      if (matchedInputTr.length < 1) {
+      if (frameData.status === 400) {
         console.error("Couldn't find the given move.");
         if (!slashCommand) {
           msg.react("❌");
@@ -128,41 +98,27 @@ module.exports = {
         );
       }
 
-      // Store all attack data
-      const attackInfo = {
-        input: matchedInputTr.find("td:eq(0)").text(),
-        hitLevel: matchedInputTr.find("td:eq(1)").text(),
-        damage: matchedInputTr.find("td:eq(2)").text(),
-        startup: matchedInputTr.find("td:eq(3)").text(),
-        block: matchedInputTr.find("td:eq(4)").text(),
-        hit: matchedInputTr.find("td:eq(5)").text(),
-        counter: matchedInputTr.find("td:eq(6)").text(),
-        notes: matchedInputTr.find("td:eq(7)").text(),
-      };
+      const { input, hit_level, damage, startup, block, hit, counter, note } =
+        frameData;
 
-      // Create response embed
-      const formattedCharacterName = characterCode.toUpperCase();
       const responseEmbed = new EmbedBuilder()
         .setColor(0x00ff00)
-        .setTitle(formattedCharacterName)
-        .setURL(rbnUrl)
-        .setDescription(`Move: ${attackInfo.input}`)
+        .setTitle(characterCode.toUpperCase())
+        .setDescription(`Move: ${input}`)
         .setFooter({
           text: `Please use the report command to submit any feedback you may have.`,
         });
 
-      // Define fields for response embed
       const fields = [
-        { name: "Hit Level", value: attackInfo.hitLevel, inline: true },
-        { name: "Damage", value: attackInfo.damage, inline: true },
-        { name: "Startup", value: attackInfo.startup, inline: true },
-        { name: "Block", value: attackInfo.block, inline: true },
-        { name: "Hit", value: attackInfo.hit, inline: true },
-        { name: "Counter", value: attackInfo.counter, inline: true },
-        { name: "Notes", value: attackInfo.notes },
+        { name: "Hit Level", value: hit_level, inline: true },
+        { name: "Damage", value: damage, inline: true },
+        { name: "Startup", value: startup, inline: true },
+        { name: "Block", value: block, inline: true },
+        { name: "Hit", value: hit, inline: true },
+        { name: "Counter", value: counter, inline: true },
+        { name: "Notes", value: note },
       ];
 
-      // Remove any fields with no value and add the remaining ones
       fields.forEach((field) => {
         if (field.value !== undefined && field.value !== "") {
           responseEmbed.addFields({
@@ -173,10 +129,10 @@ module.exports = {
         }
       });
 
-      // Finally, respond with the generated embed
       if (!slashCommand) {
         msg.react("✅");
       }
+
       respond(msg, { embeds: [responseEmbed] }, slashCommand);
       return;
     } catch (error) {
@@ -188,48 +144,7 @@ module.exports = {
 };
 
 function respond(msg, reply, slashCommand) {
-  console.log("Reply sent!");
+  console.log("Sending reply!");
   if (slashCommand == true) return msg.editReply(reply);
   else return msg.reply(reply);
-}
-
-// Function to format tekken notation to allow for easier parsing
-function formatNotation(inputNotation, removePlus) {
-  let modifiedNotation = inputNotation
-    .toLowerCase()
-
-    // TODO: This is kinda stupid, maybe move the input shortcuts to a different function
-    .replaceAll("cd", "f,n,d,df")
-    .replaceAll("debug", "b,db,d,df")
-    .replaceAll("gs", "f,n,b,db,d,df,f")
-    .replaceAll("wr", "f,f,f")
-    .replaceAll("qcf", "d,df,f")
-    .replaceAll("qcb", "d,db,b")
-    .replaceAll("hcf", "b,db,f,df,f")
-    .replaceAll("hcb", "f,df,d,db,d")
-    .replaceAll(/ *\([^)]*\) */g, "")
-    .split("or")
-    .pop()
-    .split("in rage")
-    .pop()
-    .replace(/[\s,:/()]/g, "");
-
-  if (removePlus) {
-    modifiedNotation = modifiedNotation.replaceAll(/\+/g, "");
-  }
-
-  return modifiedNotation;
-}
-
-// Check the given <tr> element for any <td> that match the given input
-function checkForInput($, unformattedInputs, removePlus = false) {
-  const formattedInputs = formatNotation(unformattedInputs, removePlus);
-  const attackRow = $("tr:has(td)")
-    .filter((index, element) => {
-      const tdText = $(element).find("td").first().text().trim();
-      return formatNotation(tdText, removePlus) == formattedInputs;
-    })
-    .first();
-
-  return attackRow;
 }
