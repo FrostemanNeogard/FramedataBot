a2f66eb750c0b78eb9016c957d6f23a3b215ae97
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-01-24 01:38:00 +0100

    Improve wording on old "fd" command

--- commands/tekken/old_fd.js
+++ commands/tekken/old_fd.js
@@ -7,18 +7,16 @@ module.exports = {
   devCommand: false,
   data: new SlashCommandBuilder()
     .setName("fd")
-    .setDescription(
-      "(DEPRACATED) Responds with information about new commands."
-    ),
+    .setDescription("(OBSOLETE) Responds with information about new commands."),
   async execute(interaction) {
     await interaction.reply(
-      `This command has been depracated. Please use the new "fd7" or "fd8" commands for Tekken 7 and Tekken 8 respectively.`
+      `This command has been rendered obsolete. Please use the new "fd7" or "fd8" commands for Tekken 7 and Tekken 8 respectively.`
     );
     return;
   },
   run: async ({ client, msg, args }) => {
     await msg.reply(
-      `This command has been depracated. Please use the new "fd7" or "fd8" commands for Tekken 7 and Tekken 8 respectively.`
+      `This command has been rendered obsolete. Please use the new "fd7" or "fd8" commands for Tekken 7 and Tekken 8 respectively.`
     );
     return;
   },
