a9c951c928fcb7b8e1a16a85b143e5d87983e756
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-02-15 00:38:04 +0100

    Temporarily remove disallowed intent

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
+    // GatewayIntentBits.MessageContent,
   ],
 });
 
