3c2e833470b2471063239e21457d93c8375ab984
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-14 14:43:17 +0200

    add messageCreate event listener to allow for simple command syntax

---
+++ src/events/events.ts
@@ -0,0 +1,26 @@
+import { ArgsOf, Discord, On } from "discordx";
+import "dotenv/config";
+import { Framedata } from "../commands/framedata";
+const { CLIENT_ID } = process.env;
+
+@Discord()
+export class EventListener {
+  @On({
+    event: "messageCreate",
+  })
+  onMessage([message]: ArgsOf<"messageCreate">) {
+    if (!message.content.startsWith(`<@${CLIENT_ID}>`)) return;
+    const args = message.content.split(" ");
+    if (args.length <= 2) {
+      message.reply(`Invalid argument amount`);
+      return;
+    }
+    const character = args[1];
+    const inputs = args.slice(2, args.length);
+    const responseEmbed = Framedata.getFrameDataEmbedBuilder(
+      character,
+      inputs.join(" ")
+    );
+    message.reply({ embeds: [responseEmbed] });
+  }
+}
