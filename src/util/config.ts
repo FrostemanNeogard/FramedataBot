118d3f665940e671cb2a6dd6d170885b9c2180fa
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-15 12:14:40 +0200

    create config with colors and embed field default data

---
+++ src/util/config.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  success: 0x00ff00,
+  warning: 0xffff00,
+  danger: 0xff0000,
+};
+
+export const EMBED_FIELDS = {
+  footer: "Please use the report command to submit any feedback you may have.",
+};
