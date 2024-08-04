4a63d2dcd95a641a1f6c3e342f4257608df46400
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-08-04 22:48:50 +0200

    add logging for command usage

--- src/events/events.ts
+++ src/events/events.ts
@@ -22,6 +22,14 @@ export class EventListener {
     }
     const character = args[1];
     const inputs = args.slice(2, args.length).join(" ");
+
+    console.log(
+      `\nShortcut: "/fd8 character:${character} move:${inputs} "\n\bwas run by: "${
+        message.member?.displayName
+      }" \n\bin channel: "${message.guildId}" \n\bin server: "${
+        message.guild?.name
+      }" \n\bat: ${new Date().toLocaleDateString()}\n`
+    );
     Framedata.getFramedataEmbed(character, inputs, "tekken8").then((response) =>
       message.reply(response)
     );
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { Events, IntentsBitField, Interaction } from "discord.js";
 import { Client } from "discordx";
 import { dirname, importx } from "@discordx/importer";
+import { time } from "console";
 
 const { TOKEN, DEV, TEST_GUILD_ID } = process.env;
 
@@ -17,6 +18,13 @@ client.once(Events.ClientReady, async () => {
 });
 
 client.on(Events.InteractionCreate, (interaction: Interaction) => {
+  console.log(
+    `\nCommand: "${interaction.toString()}"\n\bwas run by: "${
+      interaction.user.globalName
+    }" \n\bin channel: "${interaction.channelId}" \n\bin server: "${
+      interaction.guild?.name
+    }" \n\bat: ${new Date().toLocaleDateString()}\n`
+  );
   client.executeInteraction(interaction);
 });
 
