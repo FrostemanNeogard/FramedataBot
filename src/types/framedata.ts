f2937c2a817a0f543c9f969164b0f2f6a1347694
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-10-28 20:15:38 +0100

    Create TekkenFramedata type

---
+++ src/types/framedata.ts
@@ -0,0 +1,13 @@
+export type TekkenFramedata = {
+  name: string;
+  input: string;
+  hit_level: string;
+  damage: string;
+  startup: string;
+  block: string;
+  hit: string;
+  counter: string;
+  alternateInputs: string[];
+  categories: string[];
+  notes: string[];
+};
