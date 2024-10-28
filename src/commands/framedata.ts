ea99cfc980ec77c62f9d7b245d1e95a1dc27379f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-10-28 20:16:23 +0100

    Begin implementing "similar moves" reactions

--- src/commands/framedata.ts
+++ src/commands/framedata.ts
@@ -2,9 +2,11 @@ import {
   CommandInteraction,
   SlashCommandBuilder,
   SlashCommandStringOption,
+  User,
 } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
 import { FramedataService } from "../service/framedataService";
+import { MoveNotFoundError } from "../exceptions/similarMoves";
 
 const fd7Command = new SlashCommandBuilder()
   .setName("fd7")
@@ -70,7 +72,62 @@ export class Framedata {
       );
       interaction.editReply(response);
     } catch (e) {
-      console.log("An error ocurred when attempting to execute t8 command:", e);
+      if (e instanceof MoveNotFoundError) {
+        console.log("STart of thing here");
+        const similarMovesEmbed = e.getEmbed();
+        const similarMoves = e.getSimilarMoves();
+
+        interaction.editReply({ embeds: [similarMovesEmbed] });
+        const message = await interaction.fetchReply();
+
+        const reactions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"];
+        for (let i = 0; i < similarMoves.length; i++) {
+          message.react(reactions[i]);
+        }
+
+        const collectorFilter = (reaction: any, user: User) => {
+          return (
+            reactions.includes(reaction.emoji.name) &&
+            user.id === interaction.user.id
+          );
+        };
+
+        message
+          .awaitReactions({
+            filter: collectorFilter,
+            max: 1,
+            time: 120_000,
+            errors: ["time"],
+          })
+          .then((collectedReactions) => {
+            const reaction = collectedReactions.first();
+
+            if (reaction?.emoji.name == "1️⃣") {
+              console.log("One!!!!");
+              interaction.editReply("Test");
+            } else {
+              console.log("Idk what happened", reaction?.emoji.name);
+              interaction.editReply("Oh no");
+            }
+          })
+          .catch((collectedReactions) => {
+            interaction.reply("Something went wrong. Please try again.");
+            console.log(
+              `Something went wrong when reacting with a ${
+                collectedReactions.first()?.emoji.name
+              }`
+            );
+          });
+
+        // switch (collectedReactions.first()?.emoji.name) {
+        //   case reactions[0]:
+        //     interaction.editReply("Test thing");
+        //     break;
+        //   default:
+        //     message.reply("You reacted with an invalid emoji.");
+        //     break;
+        // }
+      }
     }
   }
 }
